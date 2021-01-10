882368c13a5294e6d4c88dc65649687883aec3cb
lhs-jpeg <71975304+lhs-jpeg@users.noreply.github.com>
2021-01-10 15:40:36 +0300

    Add files via upload

---
+++ 03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float a, b, ans;
+    printf("a = ");
+    scanf("%f", &a);
+    printf("b = ");
+    scanf("%f", &b);
+    ans = (a + b)/2;
+    printf("ans = %f", ans);
+    return 0;
+}
