e8ae37726416c34ac6d7fd6be9fb2352a34cda11
lhs-jpeg <71975304+lhs-jpeg@users.noreply.github.com>
2021-01-10 15:32:56 +0300

    Add files via upload

---
+++ 02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float d, L;
+    printf("d = ");
+    scanf("%f", &d);
+    float pi;
+    pi = 3.14;
+    L = pi*d;
+    printf("L = %f", L);
+    return 0;
+}
